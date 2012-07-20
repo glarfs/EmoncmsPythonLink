216e74e22bda293042ebef56ec5bd9cb1a3be2d0
Glyn Hudson <glyn.hudson@gmail.com>
2012-07-21 00:48:01 +0100

    fixed pylink.py, now works to post to emoncms, also prints out serial to terminal

--- pylink.py
+++ pylink.py
@@ -1,8 +1,10 @@
-import serial
+import serial, sys
 import httplib
 
 conn = httplib.HTTPConnection("localhost")
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 while 1:
+	sys.stdout.write(ser.readline())	
 	conn.request("GET", "/emoncms3/api/post?apikey=xxxxxxxxxxxxxxxxxxxxxxxxxxx&json="+ser.readline())
+	response = conn.getresponse()
